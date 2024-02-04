8ab041324af449a466825dbbf254f9427f448142
ihab-boudargham <ihab.boudargham@hotmail.com>
2024-02-04 14:54:30 +0200

    React

---
+++ README/01-REACT.md
@@ -0,0 +1,7 @@
+1. $ npx create-react-app frontend --template typescript
+
+2. $ npm install
+
+3. npm start
+
+4. In App.tsx, we can created our header, main and footer.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>ShopTech</header>
+
+      <main></main>
+
+      <footer>All rights are reserved</footer>
+    </div>
+  );
+}
+
+export default App;
